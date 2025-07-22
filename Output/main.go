0604920f4fed398be281841a6ebc6d62caec9d33
steven carpenter <steven.carpenter@skdevstudios.com>
2025-07-22 03:42:12 -0400

    removed test output

--- Output/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "specCon18/test-test/cmd"
-
-func main() {
-	cmd.Execute()
-}
--- Output/tui/view.go
+++
@@ -1,7 +0,0 @@
-package tui
-
-import "fmt"
-
-func (m model) View() string {
-	return "Counter: " + fmt.Sprintf("%d", m.count) + "\n[↑/↓ to change, q to quit]"
-}
