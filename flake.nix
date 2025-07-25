bf78461bbb4a6d752f3787c5f3a739eb1e25e50f
steven carpenter <steven.carpenter@skdevstudios.com>
2025-07-25 15:38:34 -0400

    fixed project name in flake

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
           default = pkgs.callPackage (
             { buildGoModule }:
             buildGoModule {
-              pname = "wand-templater";
+              pname = "bubblewand";
               version = "0.0.1";
               src = builtins.path {
                 name = "source";
